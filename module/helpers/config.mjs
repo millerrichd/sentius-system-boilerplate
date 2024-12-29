c5e69a613d83de5e87b90e63b601d92ea0a86bdd
Richard Miller <millerrichd@gmail.com>
2024-12-29 14:02:27 -0800

    initial commit after boilerplate

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const SENTIUS = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+SENTIUS.abilities = {
+  str: 'SENTIUS.Ability.Str.long',
+  dex: 'SENTIUS.Ability.Dex.long',
+  con: 'SENTIUS.Ability.Con.long',
+  int: 'SENTIUS.Ability.Int.long',
+  wis: 'SENTIUS.Ability.Wis.long',
+  cha: 'SENTIUS.Ability.Cha.long',
+};
+
+SENTIUS.abilityAbbreviations = {
+  str: 'SENTIUS.Ability.Str.abbr',
+  dex: 'SENTIUS.Ability.Dex.abbr',
+  con: 'SENTIUS.Ability.Con.abbr',
+  int: 'SENTIUS.Ability.Int.abbr',
+  wis: 'SENTIUS.Ability.Wis.abbr',
+  cha: 'SENTIUS.Ability.Cha.abbr',
+};
